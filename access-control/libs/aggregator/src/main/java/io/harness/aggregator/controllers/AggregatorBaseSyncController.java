08f267fc58d15355db7cb11fce3f1db5423ccbc2
Karan Siwach <karan.siwach@harness.io>
2022-03-29 17:27:37 +0530

    [PL-23778]: build fix

--- access-control/libs/aggregator/src/main/java/io/harness/aggregator/controllers/AggregatorBaseSyncController.java
+++ access-control/libs/aggregator/src/main/java/io/harness/aggregator/controllers/AggregatorBaseSyncController.java
@@ -17,6 +17,7 @@ import io.harness.accesscontrol.roleassignments.persistence.RoleAssignmentDBO;
 import io.harness.accesscontrol.roleassignments.persistence.repositories.RoleAssignmentRepository;
 import io.harness.accesscontrol.roles.persistence.RoleDBO;
 import io.harness.accesscontrol.roles.persistence.repositories.RoleRepository;
+import io.harness.accesscontrol.scopes.core.ScopeService;
 import io.harness.aggregator.AggregatorConfiguration;
 import io.harness.aggregator.DebeziumConfig;
 import io.harness.aggregator.MongoOffsetBackingStore;
@@ -112,7 +113,7 @@ public abstract class AggregatorBaseSyncController implements Runnable {
       PersistentLocker persistentLocker, ChangeEventFailureHandler changeEventFailureHandler,
       AggregatorJobType aggregatorJobType, ChangeConsumerService changeConsumerService,
       RoleAssignmentCRUDEventHandler roleAssignmentCRUDEventHandler,
-      UserGroupCRUDEventHandler userGroupCRUDEventHandler) {
+      UserGroupCRUDEventHandler userGroupCRUDEventHandler, ScopeService scopeService) {
     ChangeConsumer<RoleAssignmentDBO> roleAssignmentChangeConsumer = new RoleAssignmentChangeConsumerImpl(
         aclRepository, roleAssignmentRepository, changeConsumerService, roleAssignmentCRUDEventHandler);
     ChangeConsumer<RoleDBO> roleChangeConsumer = new RoleChangeConsumerImpl(
