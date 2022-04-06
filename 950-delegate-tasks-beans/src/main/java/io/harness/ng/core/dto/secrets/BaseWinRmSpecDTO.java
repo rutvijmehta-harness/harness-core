7f521195f32322311a9e5da988560e80a8c0412c
Vitalie Safronovici <vitalie.safronovici@harness.io>
2022-04-06 18:42:17 +0300

    [feat]: [CDS-36017]: Add WinRm credentials in NG

--- 950-delegate-tasks-beans/src/main/java/io/harness/ng/core/dto/secrets/BaseWinRmSpecDTO.java
+++ 950-delegate-tasks-beans/src/main/java/io/harness/ng/core/dto/secrets/BaseWinRmSpecDTO.java
@@ -7,25 +7,20 @@
 
 package io.harness.ng.core.dto.secrets;
 
-import io.harness.ng.core.models.SecretSpec;
+import io.harness.ng.core.models.BaseWinRmSpec;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.Optional;
 
 @JsonTypeInfo(
     use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type", visible = true)
 @JsonSubTypes(value =
     {
-      @JsonSubTypes.Type(value = SecretTextSpecDTO.class, name = "SecretText")
-      , @JsonSubTypes.Type(value = SecretFileSpecDTO.class, name = "SecretFile"),
-          @JsonSubTypes.Type(value = SSHKeySpecDTO.class, name = "SSHKey"),
-          @JsonSubTypes.Type(value = WinRmCredentialsSpecDTO.class, name = "WinRmCredentials"),
+      @JsonSubTypes.Type(value = NTLMConfigDTO.class, name = "NTLM")
+      , @JsonSubTypes.Type(value = KerberosConfigDTO.class, name = "Kerberos"),
     })
-@Schema(name = "SecretSpec", description = "This has details of the Secret defined in Harness.")
-public abstract class SecretSpecDTO {
-  public abstract Optional<String> getErrorMessageForInvalidYaml();
-
-  public abstract SecretSpec toEntity();
+@Schema(name = "BaseWinRmSpec", description = "This is the WinRm specification details as defined in Harness.")
+public abstract class BaseWinRmSpecDTO {
+  public abstract BaseWinRmSpec toEntity();
 }
