5e09f912e3b34be7ceb07f8b2f494233289f6cea
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2022-04-07 16:24:21 +0530

    [CDP-35484]: integrated instance clean up migration logic

--- 390-db-migration/src/main/java/io/harness/migrations/all/UpdateCorruptedInstanceStatsMigration.java
+++ 390-db-migration/src/main/java/io/harness/migrations/all/UpdateCorruptedInstanceStatsMigration.java
@@ -8,17 +8,17 @@
 package io.harness.migrations.all;
 
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static io.harness.persistence.HQuery.excludeAuthority;
 
 import static java.lang.String.format;
-import static org.mongodb.morphia.aggregation.Accumulator.accumulator;
-import static org.mongodb.morphia.aggregation.Group.grouping;
-import static org.mongodb.morphia.aggregation.Group.id;
 import static org.reflections.Reflections.log;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.FeatureName;
+import io.harness.ff.FeatureFlagService;
 import io.harness.migrations.Migration;
-import io.harness.mongo.index.migrator.ContainerIdAggregate;
+import io.harness.mongo.MongoPersistence;
 import io.harness.persistence.HIterator;
 
 import software.wings.beans.EntityType;
@@ -28,28 +28,78 @@ import software.wings.beans.infrastructure.instance.stats.InstanceStatsSnapshot;
 import software.wings.dl.WingsPersistence;
 import software.wings.service.intfc.instance.stats.InstanceStatService;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.commons.lang3.StringUtils;
 import org.mongodb.morphia.query.Query;
 
 @OwnedBy(HarnessTeam.CDP)
 public class UpdateCorruptedInstanceStatsMigration implements Migration {
   @Inject private InstanceStatService instanceStatService;
+  @Inject private MongoPersistence mongoPersistence;
+  @Inject private FeatureFlagService featureFlagService;
   @Inject private WingsPersistence wingsPersistence;
 
   // time of 21.02.2022 00:00:00 IST
-  public static final Instant FROM = Instant.ofEpochMilli(1645381800000L);
-  private final String DEBUG_LINE = "UPDATE_CORRUPTED_INSTANCE_STATS_MIGRATION: ";
+  private static final Instant FROM = Instant.ofEpochMilli(1645381800000L);
 
+  // time of 08.03.2022 00:00:00 GMT
+  private static final long CREATED_AT_FROM_TIMESTAMP = 1646697600000L;
+  // time of 23.03.2022 00:00:00 GMT
+  private static final long CREATED_AT_TILL_TIMESTAMP = 1647993600000L;
+
+  private final String DEBUG_LINE = "UPDATE_CORRUPTED_INSTANCE_STATS_MIGRATION: ";
   @Override
   public void migrate() {
     log.info("Running UpdateCorruptedInstanceStatsMigration");
+    deleteDuplicateInstanceMigration();
+    updateCorruptedInstanceStats();
+    log.info("Completed UpdateCorruptedInstanceStatsMigration");
+  }
+
+  private void deleteDuplicateInstanceMigration() {
+    try (HIterator<InfrastructureMapping> infraMappingIter =
+             new HIterator<>(mongoPersistence.createQuery(InfrastructureMapping.class, excludeAuthority)
+                                 .filter(InfrastructureMapping.InfrastructureMappingKeys.deploymentType, "HELM")
+                                 .project("_id", true)
+                                 .project(InfrastructureMapping.InfrastructureMappingKeys.accountId, true)
+                                 .project(InfrastructureMapping.InfrastructureMappingKeys.appId, true)
+                                 .disableValidation()
+                                 .fetch())) {
+      while (infraMappingIter.hasNext()) {
+        InfrastructureMapping infraMapping = infraMappingIter.next();
+        if (featureFlagService.isEnabled(FeatureName.KEEP_PT_AFTER_K8S_DOWNSCALE, infraMapping.getAccountId())) {
+          continue;
+        }
+
+        log.info("Delete orphaned instances for infra mapping {} and account {}", infraMapping.getUuid(),
+            infraMapping.getAccountId());
+        BulkWriteOperation instanceWriteOperation =
+            mongoPersistence.getCollection(Instance.class).initializeUnorderedBulkOperation();
+        HashMap<String, Object> query = new HashMap<>();
+        query.put(Instance.InstanceKeys.appId, infraMapping.getAppId());
+        query.put(Instance.InstanceKeys.infraMappingId, infraMapping.getUuid());
+        query.put(Instance.InstanceKeys.createdAt,
+            new BasicDBObject(ImmutableMap.of("$gte", CREATED_AT_FROM_TIMESTAMP, "$lt", CREATED_AT_TILL_TIMESTAMP)));
+        query.put(Instance.InstanceKeys.lastWorkflowExecutionId, null);
+        query.put(Instance.InstanceKeys.lastDeployedById, "AUTO_SCALE");
+
+        instanceWriteOperation.find(new BasicDBObject(query)).remove();
+
+        log.info("Deleted result {} for infraMapping {}", instanceWriteOperation.execute(), infraMapping.getUuid());
+      }
+    }
+  }
+
+  private void updateCorruptedInstanceStats() {
     Query<InfrastructureMapping> accountIdsQuery = wingsPersistence.createQuery(InfrastructureMapping.class)
                                                        .filter("deploymentType", "HELM")
                                                        .project("accountId", true)
@@ -60,11 +110,10 @@ public class UpdateCorruptedInstanceStatsMigration implements Migration {
         affectedAccounts.add(mapping.getAccountId());
       }
     } catch (Exception ex) {
-      log.error(StringUtils.join(DEBUG_LINE, "Error fetching affected accounts for migration"));
+      log.error(StringUtils.join(DEBUG_LINE, "Error fetching affected accounts for migration"), ex);
     }
 
     Instant to = Instant.now();
-
     affectedAccounts.forEach(accountId -> {
       try {
         List<InstanceStatsSnapshot> instanceStats = instanceStatService.aggregate(accountId, FROM, to);
@@ -73,7 +122,7 @@ public class UpdateCorruptedInstanceStatsMigration implements Migration {
           int updatedTotal = 0;
           for (InstanceStatsSnapshot.AggregateCount aggregateCount : statsSnapshot.getAggregateCounts()) {
             if (aggregateCount.getEntityType().equals(EntityType.APPLICATION)) {
-              int countPerApp =
+              long countPerApp =
                   getCorrectUniqueInstanceCount(accountId, aggregateCount.getId(), statsSnapshot.getTimestamp());
               if (countPerApp == -1) {
                 log.error(StringUtils.join(DEBUG_LINE,
@@ -82,7 +131,7 @@ public class UpdateCorruptedInstanceStatsMigration implements Migration {
                 continue;
               }
               updatedAggregates.add(new InstanceStatsSnapshot.AggregateCount(
-                  EntityType.APPLICATION, aggregateCount.getName(), aggregateCount.getId(), countPerApp));
+                  EntityType.APPLICATION, aggregateCount.getName(), aggregateCount.getId(), (int) countPerApp));
               updatedTotal += countPerApp;
             }
           }
@@ -94,83 +143,48 @@ public class UpdateCorruptedInstanceStatsMigration implements Migration {
               wingsPersistence.updateField(InstanceStatsSnapshot.class, statsSnapshot.getUuid(), "total", updatedTotal);
             } catch (Exception ex) {
               log.error(StringUtils.join(
-                  DEBUG_LINE, format("Error updating corrupted instanceStatId %s ", statsSnapshot.getUuid())));
+                            DEBUG_LINE, format("Error updating corrupted instanceStatId %s ", statsSnapshot.getUuid())),
+                  ex);
             }
           }
         }
 
       } catch (Exception ex) {
         log.error(StringUtils.join(
-            DEBUG_LINE, format("Error updating corrupted instance stats for accountId %s ", accountId)));
+                      DEBUG_LINE, format("Error updating corrupted instance stats for accountId %s ", accountId)),
+            ex);
       }
 
       log.info(StringUtils.join(
           DEBUG_LINE, format("Updated corrupted instance stats successfully for accountId %s ", accountId)));
     });
-
-    log.info("Completed UpdateCorruptedInstanceStatsMigration");
   }
 
-  private int getCorrectUniqueInstanceCount(String accountId, String appId, Instant toTime) {
+  private long getCorrectUniqueInstanceCount(String accountId, String appId, Instant toTime) {
     if (StringUtils.isBlank(accountId) || StringUtils.isBlank(appId)) {
       return 0;
     }
     try {
-      AtomicInteger containerCounter = getContainerTypeInstanceCount(accountId, appId, toTime);
-      AtomicInteger nonContainerCounter = getNonContainerTypeInstanceCount(accountId, appId, toTime);
-      return containerCounter.get() + nonContainerCounter.get();
+      return getContainerTypeInstanceCount(accountId, appId, toTime);
     } catch (Exception ex) {
-      log.error(StringUtils.join(
-          DEBUG_LINE, format("Error getting unique instance count for accountId %s, appId %s ", accountId, appId)));
+      log.error(StringUtils.join(DEBUG_LINE,
+                    format("Error getting unique instance count for accountId %s, appId %s ", accountId, appId)),
+          ex);
       return -1;
     }
   }
 
-  private AtomicInteger getNonContainerTypeInstanceCount(String accountId, String appId, Instant toTime) {
-    AtomicInteger counter = new AtomicInteger();
-    Query<Instance> nonContainerFetchInstancesQuery = wingsPersistence.createQuery(Instance.class)
-                                                          .filter("accountId", accountId)
-                                                          .filter("appId", appId)
-                                                          .filter("isDeleted", false)
-                                                          .filter("containerInstanceKey", null)
-                                                          .field("deletedAt")
-                                                          .greaterThanOrEq(toTime.toEpochMilli())
-                                                          .field("createdAt")
-                                                          .lessThanOrEq(toTime.toEpochMilli())
-                                                          .project("containerInstanceKey", true);
-
-    wingsPersistence.getDatastore(Instance.class)
-        .createAggregation(Instance.class)
-        .match(nonContainerFetchInstancesQuery)
-        .group(id(grouping("containerId", "containerInstanceKey.containerId"),
-                   grouping("namespace", "containerInstanceKey.namespace")),
-            grouping("count", accumulator("$sum", 1)))
-        .aggregate(ContainerIdAggregate.class)
-        .forEachRemaining(instance -> { counter.getAndAdd(instance.getCount()); });
-    return counter;
-  }
-
-  private AtomicInteger getContainerTypeInstanceCount(String accountId, String appId, Instant toTime) {
-    AtomicInteger counter = new AtomicInteger();
+  private long getContainerTypeInstanceCount(String accountId, String appId, Instant toTime) {
     Query<Instance> containerFetchInstancesQuery = wingsPersistence.createQuery(Instance.class)
                                                        .filter("accountId", accountId)
                                                        .filter("appId", appId)
                                                        .filter("isDeleted", false)
-                                                       .filter("containerInstanceKey !=", null)
                                                        .field("deletedAt")
                                                        .greaterThanOrEq(toTime.toEpochMilli())
                                                        .field("createdAt")
                                                        .lessThanOrEq(toTime.toEpochMilli())
-                                                       .project("containerInstanceKey", true);
+                                                       .project("_id", true);
 
-    wingsPersistence.getDatastore(Instance.class)
-        .createAggregation(Instance.class)
-        .match(containerFetchInstancesQuery)
-        .group(id(grouping("containerId", "containerInstanceKey.containerId"),
-                   grouping("namespace", "containerInstanceKey.namespace")),
-            grouping("count", accumulator("$sum", 1)))
-        .aggregate(ContainerIdAggregate.class)
-        .forEachRemaining(instance -> { counter.getAndIncrement(); });
-    return counter;
+    return containerFetchInstancesQuery.count();
   }
 }
