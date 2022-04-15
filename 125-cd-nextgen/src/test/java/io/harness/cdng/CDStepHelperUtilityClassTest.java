4e134325726cf09733d571f9457af8f0ecc05c5b
Vasile Lica <vasile.lica@harness.io>
2022-04-15 18:13:25 +0300

    [feat]: [CDS-36354]: adding unit tests for CDStepHelper

--- 125-cd-nextgen/src/test/java/io/harness/cdng/CDStepHelperUtilityClassTest.java
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/CDStepHelperUtilityClassTest.java
@@ -1,10 +1,14 @@
 package io.harness.cdng;
 
+import static io.harness.rule.OwnerRule.CDP;
 import static io.harness.rule.OwnerRule.VLICA;
+
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 
 import io.harness.CategoryTest;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
 import io.harness.cdng.manifest.yaml.GitStoreConfig;
 import io.harness.cdng.manifest.yaml.GithubStore;
@@ -22,31 +26,32 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Validator.class})
+@OwnedBy(HarnessTeam.CDP)
 public class CDStepHelperUtilityClassTest extends CategoryTest {
-    @Test
-    @Owner(developers = VLICA)
-    @Category(UnitTests.class)
-    public void testValidateGitStoreConfig() {
-        io.harness.cdng.CDStepHelper cDStepHelper = new CDStepHelper();
+  @Test
+  @Owner(developers = VLICA)
+  @Category(UnitTests.class)
+  public void testValidateGitStoreConfig() {
+    io.harness.cdng.CDStepHelper cDStepHelper = new CDStepHelper();
 
-        ParameterField<String> branch = ParameterField.createValueField("branch");
-        ParameterField<String> commit = ParameterField.createValueField("test-commit-id");
+    ParameterField<String> branch = ParameterField.createValueField("branch");
+    ParameterField<String> commit = ParameterField.createValueField("test-commit-id");
 
-        GitStoreConfig gitStoreConfigBranch = GithubStore.builder().branch(branch).gitFetchType(FetchType.BRANCH).build();
+    GitStoreConfig gitStoreConfigBranch = GithubStore.builder().branch(branch).gitFetchType(FetchType.BRANCH).build();
 
-        GitStoreConfig gitStoreConfigCommit = GithubStore.builder().commitId(commit).gitFetchType(FetchType.COMMIT).build();
+    GitStoreConfig gitStoreConfigCommit = GithubStore.builder().commitId(commit).gitFetchType(FetchType.COMMIT).build();
 
-        PowerMockito.mockStatic(Validator.class);
-        PowerMockito.doNothing().when(Validator.class);
+    PowerMockito.mockStatic(Validator.class);
+    PowerMockito.doNothing().when(Validator.class);
 
-        cDStepHelper.validateGitStoreConfig(gitStoreConfigBranch);
+    cDStepHelper.validateGitStoreConfig(gitStoreConfigBranch);
 
-        PowerMockito.verifyStatic(Validator.class, times(1));
-        Validator.notEmptyCheck(eq("Branch is Empty in Git Store config"), eq("branch"));
+    PowerMockito.verifyStatic(Validator.class, times(1));
+    Validator.notEmptyCheck(eq("Branch is Empty in Git Store config"), eq("branch"));
 
-        cDStepHelper.validateGitStoreConfig(gitStoreConfigCommit);
+    cDStepHelper.validateGitStoreConfig(gitStoreConfigCommit);
 
-        PowerMockito.verifyStatic(Validator.class, times(1));
-        Validator.notEmptyCheck(eq("Commit Id is Empty in Git Store config"), eq("test-commit-id"));
-    }
+    PowerMockito.verifyStatic(Validator.class, times(1));
+    Validator.notEmptyCheck(eq("Commit Id is Empty in Git Store config"), eq("test-commit-id"));
+  }
 }
