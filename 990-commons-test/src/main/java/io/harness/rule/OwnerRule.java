28216d791bb29de45d0125e8effef6ae07c209c2
Vasile Lica <vasile.lica@harness.io>
2022-04-15 13:56:38 +0300

    [feat]: [CDS-36228]: adding more unit-tests for NG cloudformation logic (#32253)
    * [feat]: [CDS-36228]: adding more unit-tests for NG cloudformation logic
    
    * [feat]: [CDS-36228]: fixing check style issues
    
    * [feat]: [CDS-36228]: refactoring and adding OwnedBy info to test classes

--- 990-commons-test/src/main/java/io/harness/rule/OwnerRule.java
+++ 990-commons-test/src/main/java/io/harness/rule/OwnerRule.java
@@ -248,6 +248,7 @@ public class OwnerRule implements TestRule {
   public static final String NAMANG = "naman.goenka";
   public static final String BUHA = "aleksa.buha";
   public static final String SATYAM_GOEL = "satyam.goel";
+  public static final String VLICA = "vasile.lica";
   public static final String DEV_MITTAL = "devki.mittal";
   @Deprecated public static final String UNKNOWN = "unknown";
 
@@ -428,6 +429,7 @@ public class OwnerRule implements TestRule {
           .put(SATYAM_GOEL, defaultUserInfo(SATYAM_GOEL).slack("U030MT1GRFG").team(PL).build())
           .put(VAIBHAV_KUMAR, defaultUserInfo(VAIBHAV_KUMAR).slack("U02UYG4J2SU").team(CDP).build())
           .put(BUHA, defaultUserInfo(BUHA).slack("U02V35N54R0").team(CDP).build())
+          .put(VLICA, defaultUserInfo(VLICA).slack("U038TE720QL").team(CDP).build())
           .put(DEV_MITTAL, defaultUserInfo(DEV_MITTAL).slack("U032JRFUZT2").team(CI).build())
           .build();
 
