117f7be849b7afab8a0907d7e495549dcc109f20
Vasile Lica <vasile.lica@harness.io>
2022-04-17 13:48:54 +0300

    [feat]: [CDS-36354: refactoring

--- 125-cd-nextgen/src/test/java/io/harness/cdng/CDStepHelperUtilityClassTest.java
+++ 125-cd-nextgen/src/test/java/io/harness/cdng/CDStepHelperUtilityClassTest.java
@@ -26,7 +26,6 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Validator.class})
-@OwnedBy(HarnessTeam.CDP)
 public class CDStepHelperUtilityClassTest extends CategoryTest {
   @Test
   @Owner(developers = VLICA)
