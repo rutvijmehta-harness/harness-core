27eddd92d5864978cbad48a29c3f249dfae16d1f
Arvind Choudhary <arvind.choudhary@harness.io>
2022-04-19 14:16:21 +0530

    [CDS-35152]: Fixed toString to handle sorted tags; migration is pending

--- 957-cg-beans/src/main/java/software/wings/beans/AwsInstanceFilter.java
+++ 957-cg-beans/src/main/java/software/wings/beans/AwsInstanceFilter.java
@@ -8,6 +8,7 @@
 package software.wings.beans;
 
 import static io.harness.annotations.dev.HarnessTeam.CDP;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 import static io.harness.expression.Expression.ALLOW_SECRETS;
 
 import io.harness.annotations.dev.OwnedBy;
@@ -16,15 +17,15 @@ import io.harness.expression.ExpressionReflectionUtils.NestedAnnotationResolver;
 
 import com.github.reinert.jjschema.Attributes;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 
 @Builder
 @FieldNameConstants(innerTypeName = "AwsInstanceFilterKeys")
-@ToString
 @OwnedBy(CDP)
 public class AwsInstanceFilter implements NestedAnnotationResolver {
   @Attributes(title = "VPC") private List<String> vpcIds = new ArrayList<>();
@@ -54,6 +55,27 @@ public class AwsInstanceFilter implements NestedAnnotationResolver {
 
   public void setTags(List<Tag> tags) {
     this.tags = tags;
+  }
+
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder("AwsInstanceFilter(");
+    boolean joinFlag = false;
+    if (isNotEmpty(vpcIds)) {
+      stringBuilder.append("vpcIds=");
+      stringBuilder.append(vpcIds.stream().sorted().collect(Collectors.toList()));
+      joinFlag = true;
+    }
+
+    if (isNotEmpty(tags)) {
+      if (joinFlag) {
+        stringBuilder.append(", ");
+      }
+      stringBuilder.append("tags=");
+      stringBuilder.append(tags.stream().sorted(Comparator.comparing(Tag::getKey)).collect(Collectors.toList()));
+    }
+
+    stringBuilder.append(")");
+    return stringBuilder.toString();
   }
 
   @Data
